6f00a61de518f1cd2d5b7dd830afe79bd01c51d1
Yago <yagolopes777@hotmail.com>
2021-11-30 14:39:43 -0300

    listagem por horario e pdf com isncritos 02

--- resources/views/site/pages/visitacao/listagemInscritos.blade.php
+++ resources/views/site/pages/visitacao/listagemInscritos.blade.php
@@ -10,11 +10,41 @@
 @section('content')
         <a href="/pdf" target="_blank" rel="noopener noreferrer">Imprimir</a>
         <h1>Inscritos</h1>
-        <ul>
-            @forelse($visitantes_inscritos_horario as $inscritos)
-                <li>{{ $inscritos->nome_completo }}</li>
+
+        <!---->
+<div class="card-body">
+    <table class="table table-condensed">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>INSCRITO</th>
+                <th>DEPENDENTE</th>
+                <th>DEPENDENTE</th>
+                <th>VISITOU</th>
+                {{-- <th>VAGAS</th>
+                <th>TOTAL DE INSCRITOS</th> --}}
+                {{-- <th width="270">Ações</th> --}}
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($visitantes_inscritos_horario as $inscritos)
+                <tr>
+                    <td>{{ $inscritos->id }}</td>
+                    <td>{{ $inscritos->nome_completo }}</td>
+                    <td><b>{{ $inscritos->dependente_nome ?? '' }}</b></td>
+                    <td>{{ $inscritos->dependente2_nome ?? '' }}</td>
+                    {{-- <td>{{ date('d-m-Y', strtotime($inscritos->horario_visitacao_espacos_data)) }}</td> --}}
+                    <td><b>{{ $inscritos->visitou ?? '-'}}</b></td>
+                    {{-- <td style="width=10px;">
+                        <a href="/listagem/inscritos/{{$inscritos->id}}" class="btn btn-info" title="Lista em PDF">
+                            <i class="fas fa-list"></i>
+                        </a>
+                    </td> --}}
+                </tr>
             @empty
                 <li>Nenhum Inscrito Cadastrado.</li>
             @endforelse
-        </ul>
+        </tbody>
+    </table>
+</div>
 @stop
