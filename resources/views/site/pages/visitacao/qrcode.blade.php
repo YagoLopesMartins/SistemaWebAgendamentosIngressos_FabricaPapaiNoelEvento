bcf52bd5135b4c8972c3a92d78d42e89b8315fdb
Yago <yagolopes777@hotmail.com>
2021-12-03 15:20:58 -0300

    texto qrCode

--- resources/views/site/pages/visitacao/qrcode.blade.php
+++ resources/views/site/pages/visitacao/qrcode.blade.php
@@ -167,6 +167,10 @@
             <h1 class="ui header" id="texto-comprovacao">Inscrição realizada para
                 {{$row->horario_visitacao_data}} -  {{$row->horario_visitacao_hora_inicio}} 
             </h1>
+            {{-- <h1 class="ui header" id="texto-comprovacao">
+                Importante! Chegar com antecedência na estação, <br> neste horário: 
+               <b>{{$row->horario_visitacao_hora_chegada_estacao}}</b>  
+            </h1> --}}
             <img src="https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl='{{$code}}'">
             <div style="max-width: 500px;">
                 <p id="descricao-page-comprovante-text"
@@ -203,9 +207,7 @@
                 <p id="label-documento-comprovante"><b>Contato: {{ $visitante_cadastrato->contato ?? ''}}</b> </p>
                 <p id="label-documento-comprovante"><b>Deficiência: {{ $visitante_cadastrato->deficiencia ?? ''}}</b> </p>
             </div>
-
             <hr>
-
             <div id="infos-visitante-visitacao">
                 <p id="label-nome-comprovante"><b>Nome: {{ $visitante_cadastrato->dependente_nome }}</b> </p>
                 <p id="label-documento-comprovante"><b>Data Nascimento: {{ $visitante_cadastrato->dependente_data_nascimento ?? ''}}</b> </p>
@@ -216,30 +218,45 @@
                 <p id="label-documento-comprovante"><b>Data Nascimento: {{ $visitante_cadastrato->dependente2_data_nascimento ?? ''}}</b> </p>
                 <p id="label-documento-comprovante"><b>CPF: {{ $visitante_cadastrato->dependente2_cpf ?? ''}}</b> </p>
             </div>
-
         </div>
-
-
-
         <div id="observacoes-importantes">
-            <h1 class="ui header" id="texto-title">No dia do evento, lembre-se que:</h1>
+            <h1 class="ui header" id="texto-title">INFORMAÇÕES AO VISITANTE</h1>
             <div class="divider div-transparent"></div>
             <div id="div-importantes-observacoes">
                 <b>
-                    <p id="label-documento-comprovante"> ● É necessário apresentar este comprovante e documento de
-                        identificação com foto;</p>
-                    <p id="label-documento-comprovante"> ● O comprovante de agendamento é pessoal, intransferível e só é
-                        válido para o dia e horário presentes no mesmo, não sendo permitido a utilização dele em dias e
-                        horários diferentes;</p>
+                    <p id="label-documento-comprovante"> ● O local de encontro para iniciar a visitação será na Estação da 
+                        Fábrica do Papai Noel, localizada no Largo de São Sebastião (em frente à Splash Pizzaria). 
+                        A Estação da Fábrica se encontra devidamente sinalizada e será o ponto de partida da visitação;</p>
+
+                    <p id="label-documento-comprovante"> ● É necessário chegar com 20 minutos de antecedência no local citado acima;</p>
+
+                    <p id="label-documento-comprovante"> ● Ao chegar no local de encontro (Estação da Fábrica), os visitantes deverão apresentar
+                         o comprovante do agendamento para realizar a visitação na Fábrica;</p>
+
+                    <p id="label-documento-comprovante"> ● A entrada do público será feita no horário marcado do agendamento. Não haverá tolerância no horário de chegada. 
+                        Após o tempo expirado, será necessário realizar novo agendamento;</p>
+
+                    <p id="label-documento-comprovante"> ● O comprovante de agendamento só é válido para o dia e horário presentes no mesmo, não sendo permitido a 
+                        utilização dele em dias e horários diferentes;</p>
+
                     <p id="label-documento-comprovante"> ● De acordo com o Decreto do Governo do Estado do Amazonas Nº
-                        44.581 de 22 DE SETEMBRO DE 2021, é obrigatória a apresentação do cartão de vacinação da
-                        COVID-19, com o ciclo de vacinação completo - dose única ou as duas doses, tanto para visitantes
-                        brasileiros quanto para estrangeiros, exceto menores de idade que ainda não estejam contemplados
-                        na faixa etária de vacinação;</p>
+                            44.581 de 22 DE SETEMBRO DE 2021, é obrigatória a apresentação do cartão de vacinação da
+                            COVID-19, com o ciclo de vacinação completo - dose única ou as duas doses, tanto para visitantes
+                            brasileiros quanto para estrangeiros, exceto menores de idade que ainda não estejam contemplados
+                            na faixa etária de vacinação;</p>
+
+                    <h1 class="ui header" id="texto-title">ATENÇÃO:</h1>
+                    <p id="label-documento-comprovante"> ● Caso não haja a apresentação do cartão de vacinação, o visitante não poderá realizar a visitação;</p>
+                    
                     <p id="label-documento-comprovante"> ● Será proibida a entrada de visitantes com sintomas de gripe e
                         febre;</p>
-                    <p id="label-documento-comprovante"> ● Outras informações poderão ser acrescentadas ao evento
-                        principal.</p>
+                    <p id="label-documento-comprovante"> ● O uso de máscara é obrigatório na entrada e durante a visita turística assim como para 
+                        realização de registros fotográficos.</p>
+
+                    <p id="label-documento-comprovante"> ● Será estipulado o número de até 20 (vinte) visitantes (entre adultos e crianças) por horário de visitação, 
+                            com duração média de até 30 minutos cada.</p>
+                    <p id="label-documento-comprovante"> ● É terminantemente proibido o visitante (adulto ou criança) se dispersar do grupo, uma vez que todas 
+                                as visitas serão mediadas por um condutor cultural.</p>
             </div>
         </div>
     </div>
